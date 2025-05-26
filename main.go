12aef802c6361c9da920cdeaf4b2d9febb976931
nomadicGopher <11096910+nomadicGopher@users.noreply.github.com>
2025-05-25 19:29:13 -0700

    add ollama dep, improve readability. need to upgrade go

--- main.go
+++ main.go
@@ -8,73 +8,74 @@ import (
 	"os"
 	"strconv"
 	"strings"
+
+	_ "github.com/ollama/ollama"
 )
 
 func main() {
+	const defaultGoal = "Pay off debt as quickly and efficiently as possible while not straining my monthly budget."
+
 	income := flag.String("income", "", "User's monthly income (after taxes & deductions).")
-	goal := flag.String("goal", "Pay off debt as quickly and efficiently as possible while not straining my monthly budget.", "User's financial goal for AI to provide advice for accomplishing.")
+	goal := flag.String("goal", defaultGoal, "User's financial goal for AI to provide advice for accomplishing.")
 	flag.Parse()
 
 	incomeFlt := determineIncome(*income)
-	*goal = determineGoal(*goal)
+	*goal = determineGoal(*goal, defaultGoal)
 
 	promptOllama(incomeFlt, *goal)
 
 	os.Exit(0)
 }
 
+// determineIncome checks the stdIn flags for an income. If none is found then the user is prompted to enter one. Then the value is
+// stripped of special characters and assigned to a float to ensure it is valid.
 func determineIncome(income string) (incomeFlt float64) {
-	// Check if flag was passed at runtime, if so no need to prompt the user.
+	// Check if flag was passed at runtime. If so, no need to prompt the user.
 	if income == "" {
-		scanner := bufio.NewScanner(os.Stdin)
-
 		fmt.Println("What is your monthly income (after taxes & deductions)?")
+
+		scanner := bufio.NewScanner(os.Stdin)
 		if scanner.Scan() {
 			income = scanner.Text()
 		}
-
 		if err := scanner.Err(); err != nil {
 			log.Fatalln("Error reading income response: ", err)
 		}
 	}
 
 	// Verify income is a valid dollar amount by convetting to Float64.
-	func() {
-		replacer := strings.NewReplacer("$", "", ",", "")
-		income = replacer.Replace(income)
+	replacer := strings.NewReplacer("$", "", ",", "")
+	income = replacer.Replace(income)
 
-		var err error
-		incomeFlt, err = strconv.ParseFloat(income, 64)
-		if err != nil {
-			log.Fatalln("Error formatting income: ", err)
-		}
-	}()
+	var err error
+	incomeFlt, err = strconv.ParseFloat(income, 64)
+	if err != nil {
+		log.Fatalln("Error formatting income: ", err)
+	}
 
 	return incomeFlt
 }
 
-func determineGoal(goal string) string {
-	const defaultGoal = "Pay off debt as quickly and efficiently as possible while not straining my monthly budget."
-
+// determineGoal checks the stdIn flags for a non-default goal. If it's still the default then the user is prompted for a new goal
+// or to verify the default.
+func determineGoal(goal, defaultGoal string) string {
 	// Check if flag was passed at runtime, if so no need to prompt the user.
 	if goal != defaultGoal {
 		return goal
 	}
 
 	// Prompt the user for their desired financial goal.
-	func() {
-		scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("What is your financial goal? (If you like the default option, then ust press enter.)\nDefault: ", defaultGoal)
 
-		fmt.Printf("What is your financial goal? (If you like the default value of \"%s\", then just press enter.)\n", defaultGoal)
-		if scanner.Scan() {
-			goal = scanner.Text()
-		}
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		goal = scanner.Text()
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatalln("Error reading goal response: ", err)
+	}
 
-		if err := scanner.Err(); err != nil {
-			log.Fatalln("Error reading goal response: ", err)
-		}
-	}()
-
+	// User chose the default goal.
 	if goal == "" {
 		return defaultGoal
 	}
