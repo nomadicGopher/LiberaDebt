e8ed07a9e4661c8e967fea7ddb4e9111f34f02c5
nomadicGopher <11096910+nomadicGopher@users.noreply.github.com>
2025-08-29 18:14:36 -0400

    set test scripts

--- .vscode/test.sh
+++ .vscode/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rm -f ./*.md
+rm -f ./Obligation Advice*.md
 
-go run ../. -data="obligations_sample.xlsx" -income="2000" -goal="Determine a specific prioritized strategy to payoff my loan(s) and credit card(s) as quickly & efficiently as possible without straining my monthly budget " #-model="deepseek-r1:1.5b" -excludeThink=false -outDir=""
+go run ../. -data="obligations_sample.xlsx" -income="2000" -goal="Determine a specific prioritized strategy to payoff my loan(s) and credit card(s) as quickly & efficiently as possible without straining my monthly budget " #-model="" -excludeThink=false -outDir=""
